9c38ce64863f6c1406b12be653b84844f3b1b010
Harold Newman <harnew2744@gmail.com>
2025-04-04 21:59:54 +0000

    debug stuff

--- src/sol_compat_instr_execute_v1.zig
+++ src/sol_compat_instr_execute_v1.zig
@@ -20,6 +20,8 @@ const intFromInstructionError = sig.core.instruction.intFromInstructionError;
 const printPbInstrContext = @import("debug.zig").printPbInstrContext;
 const printPbInstrEffects = @import("debug.zig").printPbInstrEffects;
 
+const EMIT_LOGS = false;
+
 /// [fd] https://github.com/firedancer-io/firedancer/blob/0ad2143a9960b7daa5eb594367835d0cbae25657/src/flamenco/runtime/tests/fd_exec_sol_compat.c#L591
 /// [solfuzz-agave] https://github.com/firedancer-io/solfuzz-agave/blob/98f939ba8afcb1b7a5af4316c6085f92111b62a7/src/lib.rs#L1043
 export fn sol_compat_instr_execute_v1(
@@ -38,21 +40,24 @@ export fn sol_compat_instr_execute_v1(
     var ctx = pb.InstrContext.decode(
         in_slice,
         decode_arena.allocator(),
-    ) catch return 0;
+    ) catch |err| {
+        std.debug.print("pb.InstrContext.decode: {s}\n", .{@errorName(err)});
+        return 0;
+    };
     defer ctx.deinit();
 
     // printPbInstrContext(ctx) catch |err| {
-    //     std.debug.print("printPbInstrContext err: {s}\n", .{@errorName(err)});
+    //     std.debug.print("printPbInstrContext: {s}\n", .{@errorName(err)});
     //     return 0;
     // };
 
-    const result = executeInstrProto(allocator, ctx, false) catch |err| {
-        std.debug.print("err: {s}\n", .{@errorName(err)});
+    const result = executeInstrProto(allocator, ctx, EMIT_LOGS) catch |err| {
+        std.debug.print("executeInstrProto: {s}\n", .{@errorName(err)});
         return 0;
     };
 
     // printPbInstrEffects(result) catch |err| {
-    //     std.debug.print("printPbInstrEffects err: {s}\n", .{@errorName(err)});
+    //     std.debug.print("printPbInstrEffects: {s}\n", .{@errorName(err)});
     //     return 0;
     // };
 
@@ -61,6 +66,10 @@ export fn sol_compat_instr_execute_v1(
 
     const out_slice = out_ptr[0..out_size.*];
     if (result_bytes.len > out_slice.len) {
+        std.debug.print("out_slice.len: {d} < result_bytes.len: {d}\n", .{
+            out_slice.len,
+            result_bytes.len,
+        });
         return 0;
     }
     @memcpy(out_slice[0..result_bytes.len], result_bytes);
